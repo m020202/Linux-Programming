ad803066ca1dbceac0a7b3047f935e73814fe66b
m020202 <m020202@naver.com>
2024-10-07 12:37:33 +0900

    feat: Standard I/O Library EX

--- ex_standard_IO.c
+++ ex_standard_IO.c
@@ -1,14 +1,23 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define SIZE 512
 
 int main() {
-    ssize_t nread;
-    char buf[SIZE];
+//    ssize_t nread;
+//    char buf[SIZE];
+//
+//    while ((nread = read(0, buf, SIZE)) > 0)
+//        write(1, buf, nread);
+//
+//    exit(0);
 
-    while ((nread = read(0, buf, SIZE)) > 0)
-        write(1, buf, nread);
+    FILE *file_stream;
+    if ((file_stream = fopen("test.txt", "r+")) == NULL) {
+        printf("Could not open the test file! \n");
+        exit(0);
+    }
 
-    exit(0);
+    return 0;
 }
