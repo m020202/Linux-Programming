799df9587a2a561c557b51374f3456552f27a051
m020202 <m020202@naver.com>
2024-10-11 17:33:05 +0900

    feat: ftw() EG

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
 #include <dirent.h>
 #include <printf.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <ftw.h>
 
-void my_pwd(void);
-
-int main() {
-    my_pwd();
-}
-
-void my_pwd(void){
-    char dirname[200];
-    if (getcwd(dirname, 200) == NULL) {
-        perror("getcwd error");
+int list(const char *name, const struct stat *sptr, int type) {
+    if (type == FTW_NS) {
+        return 0;
+    }
+    if (type == FTW_F) {
+        printf("%-30s\t0%3o\n", name, sptr->st_mode & 0777);
     }
     else
-        printf("%s\n", dirname);
+        printf("%-30s*\t0%3o\n", name, sptr->st_mode & 0777);
+
+    return 0;
 }
+
+int main() {
+    ftw(".", list, 1);
+    return 0;
+}
+
