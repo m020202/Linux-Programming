d7723a3a42e961b5f7b7f9ea4d87cc7015518a94
m020202 <m020202@naver.com>
2024-10-11 11:47:28 +0900

    chore: NULL 활용

--- main.c
+++ main.c
@@ -8,14 +8,14 @@ int main() {
     if ((dp = opendir("강의 자료")) == NULL)
         return -1;
 
-    while (d = readdir(dp)) {
+    while ((d=readdir(dp)) != NULL) {
         if (d->d_ino != 0)
             printf("%s \n", d->d_name);
     }
 
     rewinddir(dp);
 
-    while (d = readdir(dp)) {
+    while ((d=readdir(dp)) != NULL) {
         if (d->d_ino != 0)
             printf("%s \n", d->d_name);
     }
