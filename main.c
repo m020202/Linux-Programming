b2d4cf59f71fa48061e26b837d3e131293fe6e3e
m020202 <m020202@naver.com>
2024-10-12 23:19:15 +0900

    feat: fork() EG

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
-#include <dirent.h>
 #include <printf.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <ftw.h>
 
 
 int main() {
-    printf("Current PID: %d\n", getpid());
-    printf("Parent PID %d\n", getppid());
+    pid_t  pid;
+    printf("Just one process so far\n");
+
+    pid = fork();
+
+    if (pid == 0)
+        printf("I'm the child\n");
+    else if (pid > 0)
+        printf("I'm the parent, child has pid %d\n", pid);
+    else
+        printf("Fork returned error code, no child\n");
 
     return 0;
 }
