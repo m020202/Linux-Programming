45ef6b579fb8a56e59d80f99d8c60ab77335905e
m020202 <m020202@naver.com>
2024-10-12 23:00:03 +0900

    feat: getpid() & getppid() EG

--- main.c
+++ main.c
@@ -4,21 +4,11 @@
 #include <sys/stat.h>
 #include <ftw.h>
 
-int list(const char *name, const struct stat *sptr, int type) {
-    if (type == FTW_NS) {
-        return 0;
-    }
-    if (type == FTW_F) {
-        printf("%-30s\t0%3o\n", name, sptr->st_mode & 0777);
-    }
-    else
-        printf("%-30s*\t0%3o\n", name, sptr->st_mode & 0777);
-
-    return 0;
-}
 
 int main() {
-    ftw(".", list, 1);
+    printf("Current PID: %d\n", getpid());
+    printf("Parent PID %d\n", getppid());
+
     return 0;
 }
 
