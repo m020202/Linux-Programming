d50b48aa20c1760ac8ca06bd5b8655a114fe6673
m020202 <m020202@naver.com>
2024-10-13 12:01:30 +0900

    feat: execl() EG

--- main.c
+++ main.c
@@ -1,26 +1,13 @@
 #include <printf.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
 
 int main() {
-    pid_t  pid;
-    printf("Just one process so far\n");
-    printf("Calling fork ...\n");
+    printf("executing ls\n");
 
-    pid = fork();
+    execl("/bin/ls", "ls", "-l", (char *) 0);
 
-    if (pid == 0) {
-        printf("I'm the child\n");
-        printf("%d\n", getppid());
-    }
-    else if (pid > 0) {
-        wait(0);
-        printf("I'm the parent, child has pid %d\n", pid);
-        printf("%d\n", getpid());
-    }
-    else
-        printf("Fork returned error code, no child\n");
+    perror("execl failed to run ls");
 
     return 0;
 }
