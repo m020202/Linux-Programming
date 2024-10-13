1f003d563be1081d086a1229a63c589e95341e98
m020202 <m020202@naver.com>
2024-10-14 08:59:59 +0900

    feat: docommand EG

--- main.c
+++ main.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int docommand(char *command) {
+    pid_t pid;
+
+    if ((pid = fork()) < 0)
+        return -1;
+
+    if (pid == 0) {
+        execl("/bin/sh", "sh", "-c", NULL);
+        perror("esxecl");
+        exit(1);
+    }
+
+    wait(0);
+
+    return 0;
+}
 
 int fatal(char *s) {
     perror(s);
