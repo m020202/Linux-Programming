25213c3c306966f19b4ff04f8c97d9203f90fe1d
m020202 <m020202@naver.com>
2024-10-17 09:10:27 +0900

    feat: Process ID EG

--- main.c
+++ main.c
@@ -1,29 +1,10 @@
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/fcntl.h>
-
-void func1() { printf("printf func1\n"); }
-void func2() { printf("printf fucn2\n"); }
-void func3() { printf("printf func3\n"); }
-void func4() { printf("printf func4\n"); }
 
 int main() {
-    pid_t pid;
-    atexit(func1);
-    atexit(func2);
-    atexit(func3);
-    atexit(func4);
-    if ((pid = fork()) < 0) {
-        perror("fork failed");
-        exit(1);
-    }
-    if (pid == 0) {
-        printf("Child process is called\n");
-        printf("Child process calls exit\n");
-        exit(0);
-    }
-    wait(NULL);
-    printf("Parent process calls exit\n");
-    exit(0);
+    printf("%d\n", getpid());
+    printf("%d\n", getuid());
+    printf("%d\n", geteuid());
+    printf("%d\n", getgid());
+    printf("%d\n", getegid());
 };
