183aefd8e8dc0f9128066da5def9d5ce3694fdc6
m020202 <m020202@naver.com>
2024-10-18 19:46:55 +0900

    feat: opendir() & readdir() & rewinddir() EG

--- main.c
+++ main.c
@@ -2,14 +2,30 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/fcntl.h>
+#include <dirent.h>
 
-int main() {
-    if (access("data", R_OK) == -1) {
-        printf("Couldn't read file");
+int my_double_ls(const char *name) {
+    struct dirent *d;
+    DIR *dp;
+
+    if ((dp = opendir("hello")) == NULL)
         return -1;
+    while ((d = readdir(dp)) != NULL) {
+        if (d->d_ino != 0)
+            printf("%s\n", d->d_name);
     }
 
-    printf("%s readable, proceeding\n", "data");
+    rewinddir(dp);
 
+    while ((d = readdir(dp))) {
+        if (d->d_ino != 0)
+            printf("%s\n", d->d_name);
+    }
+    closedir(dp);
+    return 0;
+}
+
+int main() {
+    my_double_ls("hello");
     return 0;
 };
