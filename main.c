e9a491dad98cf0a3227d56ce4355e827d249f99f
m020202 <m020202@naver.com>
2024-10-18 13:48:51 +0900

    feat: fprintf() EG

--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 
 int main() {
     FILE *file;
-    file = fopen("data", "a");
+    file = fopen("data", "w");
+    if (file == NULL) {
+        perror("파일 열기 실패: ");
+        return -1;
+    }
 
-    putc('a', file);
-    putc('b', file);
+    int age = 25;
+    const char *name = "J";
+    fprintf(file, "이름: %s\n나이: %d", name, age);
 
     return 0;
 };
