b7fc2001f0cb4d9e7fbfb68d72da6d23743da056
m020202 <m020202@naver.com>
2024-10-18 13:04:08 +0900

    feat: dup2() EG

--- main.c
+++ main.c
@@ -2,44 +2,18 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int copyfile(const char *name1, const char *name2) {
-    int infile, outfile;
-    ssize_t nread;
-    char buffer[512];
-
-    if ((infile = open(name1, O_RDONLY)) == -1)
-        return -1;
-    if ((outfile = open(name2, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1){
-        close(infile);
-        return -2;
-    }
-
-    while ((nread = read(infile, buffer, 512)) > 0) {
-        if ((write(outfile, buffer, nread)) < nread){
-            close(infile);
-            close(outfile);
-            return -3;
-        }
-    }
-
-    close(infile);
-    close(outfile);
-    if (nread == -1) return -4;
-    else return 0;
-}
-
 int main() {
-    int fd;
-    ssize_t n1, n2, n3;
-    char buf1[512], buf2[512], buf3[512];
+    int fd1, fd2;
+    if ((fd1=open("data", O_RDONLY)) == -1)
+        perror("fd1");
+    if ((fd2 = open("data2", O_WRONLY | O_CREAT, 0644)) == -1)
+        perror("fd2");
 
-    if ((fd = open("data", O_RDONLY, 0644)) == -1)
-        return -1;
+    dup2(fd1, fd2);
+    printf("%d\n", lseek(fd2, 0, SEEK_CUR));
 
-    n1 = read(fd, buf1, 512);
-    printf("%d", n1);
-
-    close(fd);
-
+    char buf[512];
+    read(fd1, buf, 10);
+    printf("%d\n", lseek(fd2, 0, SEEK_CUR));
     return 0;
 };
