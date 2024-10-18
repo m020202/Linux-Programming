04e4d8dae8cbda55e078daa03bf329ade965ba85
m020202 <m020202@naver.com>
2024-10-18 14:32:44 +0900

    feat: access() EG

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #include <sys/fcntl.h>
 
 int main() {
-    int fd;
-    fd = open("data2", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-    umask(022);
-    struct stat file_stat;
-    stat("data2", &file_stat);
-    printf("new author: %03o\n", file_stat.st_mode & 0777);
+    if (access("data", R_OK) == -1) {
+        printf("Couldn't read file");
+        return -1;
+    }
+
+    printf("%s readable, proceeding\n", "data");
 
     return 0;
 };
