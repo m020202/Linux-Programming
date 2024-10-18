482a62235d61ac0c738f4fac08d1fe16e2aa2ebd
m020202 <m020202@naver.com>
2024-10-18 13:36:27 +0900

    feat: fopen() EG

--- main.c
+++ main.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
-#include <fcntl.h>
 
 int main() {
-    int fd1, arg;
-    if ((fd1=open("data", O_WRONLY | O_APPEND)) == -1)
-        perror("fd1");
+    FILE *file;
 
-    if ((arg = fcntl(fd1, F_GETFL)) == -1) {
-        printf("File status failed\n");
+    if ((file = fopen("data","r")) == NULL) {
+        printf("Couldn't open file!\n");
         return -1;
     }
-
-    switch (arg & O_ACCMODE) {
-        case O_WRONLY:
-            printf("write-only mode");
-            break;
-        case O_RDONLY:
-            printf("read-only-mode");
-            break;
-        case O_RDWR:
-            printf("read-write");
-            break;
-        default:
-            printf("No such mode");
-    }
-    if (arg & O_APPEND)
-        printf(" with append flag set");
 
     return 0;
 };
