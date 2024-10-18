ac0adc5afebf3c3f4c34d402bc4a8d8395785b25
m020202 <m020202@naver.com>
2024-10-18 13:24:50 +0900

    feat: file mode using fcntl() EG

--- main.c
+++ main.c
@@ -2,11 +2,30 @@
 #include <fcntl.h>
 
 int main() {
-    int fd1, fd2;
-    if ((fd1=open("data", O_RDWR)) == -1)
+    int fd1, arg;
+    if ((fd1=open("data", O_WRONLY | O_APPEND)) == -1)
         perror("fd1");
 
-    fd2 = fcntl(fd1, F_DUPFD);
-    printf("%d", fd2);
+    if ((arg = fcntl(fd1, F_GETFL)) == -1) {
+        printf("File status failed\n");
+        return -1;
+    }
+
+    switch (arg & O_ACCMODE) {
+        case O_WRONLY:
+            printf("write-only mode");
+            break;
+        case O_RDONLY:
+            printf("read-only-mode");
+            break;
+        case O_RDWR:
+            printf("read-write");
+            break;
+        default:
+            printf("No such mode");
+    }
+    if (arg & O_APPEND)
+        printf(" with append flag set");
+
     return 0;
 };
