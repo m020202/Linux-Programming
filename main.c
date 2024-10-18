2d14b9ce80677023717cfb2ad6b41e16cd1904f6
m020202 <m020202@naver.com>
2024-10-18 12:08:14 +0900

    feat: file copy using read() & write() EG

--- main.c
+++ main.c
@@ -2,11 +2,44 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int copyfile(const char *name1, const char *name2) {
+    int infile, outfile;
+    ssize_t nread;
+    char buffer[512];
+
+    if ((infile = open(name1, O_RDONLY)) == -1)
+        return -1;
+    if ((outfile = open(name2, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1){
+        close(infile);
+        return -2;
+    }
+
+    while ((nread = read(infile, buffer, 512)) > 0) {
+        if ((write(outfile, buffer, nread)) < nread){
+            close(infile);
+            close(outfile);
+            return -3;
+        }
+    }
+
+    close(infile);
+    close(outfile);
+    if (nread == -1) return -4;
+    else return 0;
+}
+
 int main() {
     int fd;
+    ssize_t n1, n2, n3;
+    char buf1[512], buf2[512], buf3[512];
 
-    fd = open("data", O_WRONLY | O_CREAT, 0644);
-    printf("%d\n", lseek(fd, 0, SEEK_END));
+    if ((fd = open("data", O_RDONLY, 0644)) == -1)
+        return -1;
+
+    n1 = read(fd, buf1, 512);
+    printf("%d", n1);
+
+    close(fd);
 
     return 0;
 };
