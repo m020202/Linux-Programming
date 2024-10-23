5d5d907c4bc18156e7ad68b67d83ab57ec7ea5e4
m020202 <m020202@naver.com>
2024-10-23 10:57:56 +0900

    feat: EX: fileStatus() EG

--- main.c
+++ main.c
@@ -8,12 +8,40 @@
 #include <stdlib.h>
 #include <ulimit.h>
 
+int fileStatus(int fd) {
+    int arg1;
+
+    if ((arg1 = fcntl(fd, F_GETFL)) == -1) {
+        printf("file status failed\n");
+        return -1;
+    }
+
+    switch (arg1 & O_ACCMODE) {
+        case O_WRONLY:
+            printf("WR");
+            break;
+        case O_RDONLY:
+            printf("RD");
+            break;
+        case O_RDWR:
+            printf("RDWR");
+            break;
+        default:
+            printf("NO such mode");
+    }
+
+    if (arg1 & O_APPEND) {
+        printf("-append flag set");
+    }
+}
+
+
+
 
 int main() {
     int fd = open("data.txt", O_RDWR);
-    int newFd = fcntl(fd, F_DUPFD);
 
-    printf("%d", newFd);
+    fileStatus(fd);
 
     close(fd);
 };
