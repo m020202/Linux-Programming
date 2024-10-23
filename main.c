0fe33a19ddf4b5b784bf1ffabbfddef94b3d8114
m020202 <m020202@naver.com>
2024-10-23 10:15:56 +0900

    feat: EX: copyFile() EG

--- main.c
+++ main.c
@@ -8,15 +8,33 @@
 #include <stdlib.h>
 #include <ulimit.h>
 
+int copyFile(const char *name1, const char *name2) {
+    int infile, outfile;
+    char buf[512];
+    ssize_t nread;
+
+    if ((infile = open(name1, O_RDONLY))) {
+        return -1;
+    }
+    if ((outfile = open(name2, O_WRONLY | O_CREAT | O_TRUNC, 0644))) {
+        close(infile);
+        return -2;
+    }
+
+    while ((nread = read(infile, buf, 512)) > 0) {
+        if (write(outfile, buf, nread) < nread) {
+            close(infile);
+            close(outfile);
+            return -3;
+        }
+    }
+    close(infile);
+    close(outfile);
+    if (nread == -1) return -4;
+    else return 0;
+}
+
 
 int main() {
-    int fd;
-    ssize_t nread;
-    char buf[512];
 
-    fd = open("data", O_RDONLY);
-    nread = read(fd, buf, 512);
-    printf("%d", nread);
-
-    close(fd);
 };
