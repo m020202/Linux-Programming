63abe62a8724a7ba437d245a4ae412eea2f48153
m020202 <m020202@naver.com>
2024-10-23 13:35:14 +0900

    feat: filedata EG

--- main.c
+++ main.c
@@ -7,21 +7,40 @@
 #include <sys/statvfs.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-    if (argc != 3)
-        fprintf(stderr, "usage: move file1 file2\n");
+static short octarray[9] = {
+        0400, 0200, 0100,
+        0040, 0020, 0010,
+        0004, 0002, 0001
+};
 
-    if (link(argv[1], argv[2]) == -1){
-        perror("link failed");
-        exit(1);
+static char perms[10] = "rwxrwxrwx";
+
+int fileData(const char *path) {
+    struct stat buf;
+    int i;
+    char descrip[10];
+
+    if (stat(path, &buf) == -1) {
+        fprintf(stderr, "Couldn't stat %s\n", path);
+        return -1;
     }
 
-    if (unlink(argv[1]) == -1) {
-        perror("unlink failed");
-        unlink(argv[2]);
-        exit(1);
+    for (i = 0; i < 9; ++i) {
+        if (buf.st_mode & octarray[i])
+            descrip[i] = perms[i];
+        else
+            descrip[i] = '-';
     }
+    descrip[9] = '\0';
 
-    printf("Succeeded\n");
+    printf("\nFile %s :\n", path);
+    printf("Size %ld bytes\n", buf.st_size);
+    printf("User-id %d, Group-id %d\n\n", buf.st_uid, buf.st_gid);
+    printf("Permissions: %s\n", descrip);
 
+    return 0;
+}
+
+int main() {
+    fileData("data.txt");
 };
