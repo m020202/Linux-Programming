6a734e0bb25ccdc023f348fe3f3d93796457ee6e
m020202 <m020202@naver.com>
2024-11-11 21:23:05 +0900

    Feat: Non-Blocking Pipe EG

--- main.c
+++ main.c
@@ -2,31 +2,73 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
 
-#define MSGSIZE 16
+#define MSGSIZE 6
 
-char *msg1 = "hello world #1";
-char *msg2 = "hello world #2";
-char *msg3 = "hello world #3";
+int parent(int *);
+int child(int *);
+
+char *msg1 = "hello";
+char *msg2 = "bye!!";
 
 int main() {
-    char inBuf[MSGSIZE];
     int p[2];
-    pid_t pid;
-
     if (pipe(p) == -1) {
-        perror("pipe call: ");
-        exit(1);
+        perror("pipe call");
+        return 0;
+    }
+    if (fcntl(p[0], F_SETFL, O_NONBLOCK) == -1) {
+        perror("fcntl call");
+        return 0;
     }
 
-    write(p[1], msg1, MSGSIZE);
-    write(p[1], msg2, MSGSIZE);
-    write(p[1], msg3, MSGSIZE);
-
-    for (int i = 0; i < 3; ++i) {
-        read(p[0], inBuf, MSGSIZE);
-        printf("%s\n", inBuf);
+    switch (fork()) {
+        case -1:
+            perror("fork call");
+            exit(1);
+        case 0:
+            child(p);
+        default:
+            parent(p);
     }
-
     return 0;
+}
+
+int child(int p[2]) {
+    int count;
+    close(p[0]);
+    for (count = 0; count < 3; count++) {
+        write(p[1], msg1, MSGSIZE);
+        sleep(3);
+    }
+    write(p[1], msg2, MSGSIZE);
+    exit(0);
+}
+
+int parent(int p[2]) {
+    int nread;
+    char buf[MSGSIZE];
+    close(p[1]);
+
+    for(;;) {
+        switch (nread = read(p[0], buf, MSGSIZE)) {
+            case -1:
+                if (errno == EAGAIN) {
+                    printf("(pipe empty)\n");
+                    sleep(1);
+                    break;
+                }
+                else {
+                    perror("read call");
+                    exit(1);
+                }
+            case 0:
+                printf("End of conversation\n");
+                exit(0);
+            default:
+                printf("MSG=%s\n", buf);
+        }
+    }
 }
