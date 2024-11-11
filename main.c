7701b337a37a6becbddee4333836d5cf318120d2
m020202 <m020202@naver.com>
2024-11-11 22:34:38 +0900

    Feat: FIFO() EG

--- main.c
+++ main.c
@@ -4,49 +4,25 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
 
-#define MSGSIZE 6
+#define MSGSIZE 63
+char *fifo = "fifo";
+int main(int argc, char **argv) {
+    int fd;
+    char msgBuf[MSGSIZE + 1];
 
-int join (char *com1[], char *com2[]) {
-    int p[2], status;
-
-    switch (fork()) {
-        case -1:
-            return -1;
-        case 0:
-            break;
-        default:
-            wait(&status);
-            return status;
+    if (mkfifo(fifo, 0644) == -1) {
+        if (errno != EEXIST) return -1;
     }
 
-    if (pipe(p) == -1)
+    if ((fd = open(fifo, O_RDWR)) < 0)
         return -1;
 
-    switch (fork()) {
-        case -1:
+    for (;;) {
+        if (read(fd, msgBuf, MSGSIZE + 1) < 0)
             return -1;
-        case 0:
-            dup2(p[1], 1);
-            close(p[0]);
-            close(p[1]);
-            execvp(com1[0], com1);
-            return -1;
-        default:
-            dup2(p[0], 0);
-            close(p[0]);
-            close(p[1]);
-            execvp(com2[0], com2);
-            return -1;
+        printf("message received %s\n", msgBuf);
     }
-}
-
-int main() {
-    char *one[4] = {"ls", "-l", "/usr/lib", NULL};
-    char *two[3] = {"grep", "^d", NULL};
-    int ret;
-
-    ret = join(one, two);
-    printf("join returned %d\n", ret);
-    exit(0);
 }
