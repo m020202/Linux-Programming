1c61363e2c49d0b682f103ef97bff992a30391f3
m020202 <m020202@naver.com>
2024-11-11 22:24:10 +0900

    Feat: join() Using dup2() & pipe() EG

--- main.c
+++ main.c
@@ -7,64 +7,46 @@
 
 #define MSGSIZE 6
 
-char *msg1 = "hello";
-char *msg2 = "bye!!";
+int join (char *com1[], char *com2[]) {
+    int p[2], status;
 
-int child(int[]);
-void parent(int p[3][2]) {
-    char buf[MSGSIZE], ch;
-    fd_set set, master;
-    for (int i = 0; i < 3; ++i) close(p[i][1]);
-    FD_SET(0, &master);
-    for (int i = 0; i < 3; ++i) {
-        FD_SET(p[i][0], &master);
+    switch (fork()) {
+        case -1:
+            return -1;
+        case 0:
+            break;
+        default:
+            wait(&status);
+            return status;
     }
 
-    while (set = master, select(p[2][1]+1, &set, NULL, NULL, NULL) > 0) {
-        if (FD_ISSET(0, &set)) {
-            printf("From standard input ...");
-            read(0, &ch, 1);
-            printf("%c\n", ch);
-        }
-        for (int i = 0; i < 3; ++i) {
-            if (FD_ISSET(p[i][0], &set)) {
-                if (read(p[i][0], buf, MSGSIZE) > 0) {
-                    printf("Message from child%d\n", i);
-                    printf("MSG=%s\n", buf);
-                }
-            }
-        }
-        if (waitpid(-1, NULL, WNOHANG) == -1) return;
-    }
-}
+    if (pipe(p) == -1)
+        return -1;
 
-void fatal(char *buf) {
-    printf("%s\n", buf);
-    exit(1);
+    switch (fork()) {
+        case -1:
+            return -1;
+        case 0:
+            dup2(p[1], 1);
+            close(p[0]);
+            close(p[1]);
+            execvp(com1[0], com1);
+            return -1;
+        default:
+            dup2(p[0], 0);
+            close(p[0]);
+            close(p[1]);
+            execvp(com2[0], com2);
+            return -1;
+    }
 }
 
 int main() {
-    int pip[3][2];
-    for (int i = 0; i < 3; ++i) {
-        if (pipe(pip[i]) == -1)
-            fatal("pipe call");
-        switch (fork()) {
-            case -1:
-                fatal("fork call");
-            case 0:
-                child(pip[i]);
-        }
-    }
-    parent(pip);
-}
+    char *one[4] = {"ls", "-l", "/usr/lib", NULL};
+    char *two[3] = {"grep", "^d", NULL};
+    int ret;
 
-int child(int p[2]) {
-    close(p[0]);
-
-    for (int count = 0; count < 2; ++count) {
-        write(p[1], msg1, MSGSIZE);
-        sleep(getpid() % 4);
-    }
-    write(p[1], msg2, MSGSIZE);
+    ret = join(one, two);
+    printf("join returned %d\n", ret);
     exit(0);
 }
