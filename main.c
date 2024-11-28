5880b2eafaf7556c6dcc72d1f7af1eb52f9f2ce0
m020202 <m020202@naver.com>
2024-11-28 18:39:00 +0900

    Feat: get pipe size() EG

--- main.c
+++ main.c
@@ -7,39 +7,37 @@
 #include <errno.h>
 #include <fcntl.h>
 
-#define MSGSIZE 16
-
-char *msg1 = "hello world #1";
-char *msg2 = "hello world #2";
-char *msg3 = "hello world #3";
+int count;
+void alarm_action(int sig) {
+    printf("wrtie blocked after %d characters\n", count);
+    exit(0);
+}
 
 int main() {
-    char inBuf[MSGSIZE];
     int p[2];
-    pid_t pid;
+    int pipe_size;
+    char c = 'x';
+    static struct sigaction act;
+
+    act.sa_handler = alarm_action;
+    sigfillset(&(act.sa_mask));
 
     if (pipe(p) == -1) {
         perror("pipe call: ");
         exit(1);
     }
 
-    switch (pid = fork()) {
-        case -1:
-            perror("fork call: ");
-            exit(1);
-        case 0:
-            close(p[0]);
-            write(p[1], msg1, MSGSIZE);
-            write(p[1], msg2, MSGSIZE);
-            write(p[1], msg3, MSGSIZE);
-            break;
-        default:
-            close(p[1]);
-            for(int i =0; i < 3; ++i) {
-                read(p[0], inBuf, MSGSIZE);
-                printf("%s\n", inBuf);
-            }
-            wait(NULL);
+    pipe_size = fpathconf(p[0], _PC_PIPE_BUF);
+    printf("Maximum size of write to pipe: %dbytes\n", pipe_size);
+
+    sigaction(SIGALRM, &act, NULL);
+
+    while (1) {
+        alarm(5);
+        write(p[1], &c, 1);
+        alarm(0);
+        if ((++count % 1024) == 0)
+            printf("%d characters in pipe\n", count);
     }
 
     return 0;
