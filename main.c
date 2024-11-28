51fb27fd56708f6ceab6b41e85acc3ad52f89254
m020202 <m020202@naver.com>
2024-11-28 19:37:13 +0900

    Feat: Non-Blocking Pipe EG

--- main.c
+++ main.c
@@ -7,42 +7,62 @@
 #include <errno.h>
 #include <fcntl.h>
 
-int count;
-int time_out;
-void alarm_action(int sig) {
-    printf("\nwrite blocked after %d characters\n", count);
-    time_out = 1;
+#define MSGSIZE 6
+
+char *msg1 = "hello";
+char *msg2 = "bye";
+
+int child(int p[2]) {
+    int cnt;
+    close(p[0]);
+    for (cnt = 0; cnt < 3; ++cnt) {
+        write(p[1], msg1, MSGSIZE);
+        sleep(3);
+    }
+
+    write(p[1], msg2, MSGSIZE);
+    exit(0);
+}
+
+int parent(int p[2]) {
+    int nread;
+    char buf[MSGSIZE];
+    close(p[1]);
+    for(;;) {
+        switch (nread = read(p[0], buf, MSGSIZE)) {
+            case -1:
+                if (errno == EAGAIN) {
+                    printf("(pipe empty)\n");
+                    sleep(1);
+                    break;
+                }
+                else {
+                    perror("read call: ");
+                    exit(1);
+                }
+            case 0:
+                printf("End of Conversation\n");
+                exit(0);
+            default:
+                printf("MSG=%s\n", buf);
+        }
+    }
 }
 
 int main() {
     int p[2];
-    int pipe_size;
-    char c = 'x';
-    static struct sigaction act;
-
-    act.sa_handler = alarm_action;
-    sigfillset(&(act.sa_mask));
-
-    if (pipe(p) == -1) {
-        perror("pipe call: ");
+    if (pipe(p) == -1)
+        exit(0);
+    if (fcntl(p[0], F_SETFL, O_NONBLOCK) == -1)
         exit(1);
+
+    switch (fork()) {
+        case -1:
+            exit(1);
+        case 0:
+            child(p);
+        default:
+            parent(p);
     }
-
-    pipe_size = fpathconf(p[0], _PC_PIPE_BUF);
-    printf("Maximum size of write to pipe: %dbytes\n", pipe_size);
-
-    sigaction(SIGALRM, &act, NULL);
-
-    while (1) {
-        time_out = 0;
-        alarm(3);
-        write(p[1], &c, 1);
-        alarm(0);
-        if (time_out)
-            break;
-        if ((++count % 1024) == 0)
-            printf("%d characters in pipe\n", count);
-    }
-
     return 0;
 }
