46f0d7c108dc8557aa9c938ce5a2bec8b996e11b
m020202 <m020202@naver.com>
2024-12-03 13:10:02 +0900

    Feat: semaphore P() & V() EG

--- sender.c
+++ sender.c
@@ -9,7 +9,44 @@
 #include <sys/stat.h>
 #include <sys/msg.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 #include "pv.h"
+
+#define SEMPERM 0600
+typedef union semun {
+    int val;
+    struct semtid_ds *buf;
+    unsigned short *array;
+};
+
+int P(int semid) {
+    struct sembuf p_buf;
+    p_buf.sem_num = 0;
+    p_buf.sem_op = -1;
+    p_buf.sem_flg = SEM_UNDO;
+
+    if (semop(semid, &p_buf, 1) == -1) {
+        perror("semop failed: ");
+        return -1;
+    }
+
+    return 0;
+
+}
+
+int V(int semid) {
+    struct sembuf v_buf;
+    v_buf.sem_num = 0;
+    v_buf.sem_op = 1;
+    v_buf.sem_flg = SEM_UNDO;
+
+    if (semop(semid, &v_buf, 1) == -1) {
+        perror("semop failed: ");
+        return -1;
+    }
+
+    return 0;
+}
 
 int main() {
     int shmId;
