b1c4bbb4f91f2695a8dbbd0ab25da7ac72d5aa07
m020202 <m020202@naver.com>
2024-12-04 21:57:11 +0900

    Feat: priority 체크 위해 type 여러 종류 메시지 send

--- sender.c
+++ sender.c
@@ -12,24 +12,25 @@
 #include <sys/sem.h>
 #include "msg_header.h"
 
-
-
 int main() {
     key_t key;
     int msgid;
     struct msg_entry msg;
 
-    if ((msgid = msgget(IPC_PRIVATE, 0644 | IPC_CREAT)) == -1) {
+    if ((msgid = msgget((key_t) 3836, 0644 | IPC_CREAT)) == -1) {
         perror("msgget");
         exit(0);
     }
 
-    msg.mtype = 1;
-    strcpy(msg.mtext, "message type $1\n");
-    if (msgsnd(msgid, &msg, 100, IPC_NOWAIT) == -1) {
-        perror("msgsnd");
-        exit(1);
+    for (int i = 10; i > 0; i--) {
+        msg.mtype = 1;
+        char message[100];
+        sprintf(message, "Message Type $%d\n", i);
+        strcpy(msg.mtext, message);
+        if(msgsnd(msgid, &msg, 100, IPC_NOWAIT) == -1) {
+            perror("msgsnd");
+            exit(1);
+        }
     }
-
     return 0;
 }
