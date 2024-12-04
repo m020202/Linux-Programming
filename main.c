ba9e1d7c013ac62eb03a4c0a7193f057473d3603
m020202 <m020202@naver.com>
2024-12-04 21:48:10 +0900

    Feat: message queue EG using fork()

--- main.c
+++ main.c
@@ -8,11 +8,36 @@
 #include <fcntl.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include "msg_header.h"
 
 int main() {
-    char c;
+    int msgid;
+    struct msg_entry msg;
 
-    printf("%d", (int)sizeof(c));
+    if ((msgid = msgget(IPC_CREAT, 0644 | IPC_CREAT)) == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    switch (fork()) {
+        case -1:
+            perror("fork");
+            exit(1);
+        case 0:
+            msg.mtype = 1;
+            strcpy(msg.mtext, "message type 1 from child\n");
+            if (msgsnd(msgid, &msg, 100, IPC_NOWAIT) == -1) {
+                perror("msgsnd");
+                exit(1);
+            }
+            return 0;
+        default:
+            while(msgrcv(msgid ,&msg,100, 0,0) > 0) {
+                printf("Received Message = %s\n", msg.mtext);
+            }
+            wait(NULL);
+    }
 
     return 0;
+
 }
