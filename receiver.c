e2fc3a8fe2fc15dd24310016680117897801ecfc
m020202 <m020202@naver.com>
2024-12-07 22:28:11 +0900

    Feat: test_read using shared_memory EG

--- receiver.c
+++ receiver.c
@@ -9,9 +9,35 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
-#include "msg_header.h"
+#include "shared_memory.h"
 
 int main() {
-    union semun arg;
-    arg.array
+    int shmid;
+    SHM_INFOS *shmInfos = NULL;
+
+    shmid = shmget((key_t) 3836, sizeof(SHM_INFOS) * SHM_INFO_COUNT, 0600 | IPC_CREAT);
+
+    if (shmid == -1) {
+        perror("shmget");
+        return 0;
+    }
+    void *shared_memory = NULL;
+    shared_memory = shmat(shmid, 0, SHM_RND);
+
+    if (shared_memory == (void *) NULL) {
+        perror("shmat");
+        return 0;
+    }
+
+    shmInfos = (SHM_INFOS *) shared_memory;
+
+    while(1) {
+        for (int i = 0; i < SHM_INFO_COUNT; ++i) {
+            printf("---[%d] shared info ---\n", i);
+            printf("String IP[%s]\n", shmInfos[i].str_ip);
+            printf("String IP[%u]\n", shmInfos[i].int_ip);
+            printf("String IP[%u]\n", shmInfos[i].int_id);
+            sleep(1);
+        }
+    }
 }
