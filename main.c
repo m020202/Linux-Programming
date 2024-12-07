c6c00888dbcb4929653dea747a093d87f4fd8278
m020202 <m020202@naver.com>
2024-12-07 22:08:51 +0900

    Feat: writer()

--- main.c
+++ main.c
@@ -85,7 +85,27 @@ void reader(int semid, struct databuf buf1, struct databuf buf2) {
             return;
     }
 }
+
+void writer(int semid, struct databuf buf1, struct databuf buf2) {
+    for (;;) {
+        semop(semid, &p1, 1);
+        semop(semid, &v2, 1);
+
+        if (buf1.d_nread <= 0)
+            return;
+
+        write(1, buf1.d_buf, buf1.d_nread);
+
+        semop(semid, &p1, 1);
+        semop(semid, &v2, 1);
+
+        if (buf2.d_nread <= 0)
+            return;
+
+        write(1, buf2.d_buf, buf2.d_nread);
+    }
+}
 int main() {
-    union semun ar
+    union semun arg;
     return 0;
 }
