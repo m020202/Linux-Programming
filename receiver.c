c222f8a08f708dfddc6667129036e4e2eabc6c87
m020202 <m020202@naver.com>
2024-12-09 20:07:12 +0900

    Feat: receiver using shared_mem EG

--- receiver.c
+++ receiver.c
@@ -13,30 +13,25 @@
 
 int main() {
     int shmid;
-    SHM_INFOS *shmInfos = NULL;
+    SHM_INFO *shmInfo = NULL;
 
-    shmid = shmget((key_t) 3836, sizeof(SHM_INFOS) * SHM_INFO_COUNT, 0600 | IPC_CREAT);
-
-    if (shmid == -1) {
+    if ((shmid = shmget((key_t) 3836, sizeof(SHM_INFO) * 4, 0600 | IPC_CREAT)) == -1) {
         perror("shmget");
-        return 0;
+        exit(1);
     }
-    void *shared_memory = NULL;
-    shared_memory = shmat(shmid, 0, SHM_RND);
 
-    if (shared_memory == (void *) NULL) {
+    if ((shmInfo = (SHM_INFO *) shmat(shmid, 0, SHM_RND)) == -1) {
         perror("shmat");
-        return 0;
+        exit(1);
     }
 
-    shmInfos = (SHM_INFOS *) shared_memory;
+    while (1) {
+        for (int i = 0; i < 4; ++i) {
+            printf("%d", i);
+            printf("%s", shmInfo[i].str_ip);
+            printf("%u", shmInfo[i].int_ip);
+            printf("%u", shmInfo[i].int_id);
 
-    while(1) {
-        for (int i = 0; i < SHM_INFO_COUNT; ++i) {
-            printf("---[%d] shared info ---\n", i);
-            printf("String IP[%s]\n", shmInfos[i].str_ip);
-            printf("String IP[%u]\n", shmInfos[i].int_ip);
-            printf("String IP[%u]\n", shmInfos[i].int_id);
             sleep(1);
         }
     }
