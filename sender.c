67a2cb4fd2beb6d63b0ca165c17a5c95b91d7f13
m020202 <m020202@naver.com>
2024-12-09 20:10:13 +0900

    Feat: sender using shared_mem EG

--- sender.c
+++ sender.c
@@ -13,27 +13,24 @@
 #include "shared_memory.h"
 
 int main() {
-    int shmid;
-    SHM_INFOS *shmInfos = NULL;
+    int shmId;
+    SHM_INFO *shmInfo = NULL;
 
-    if ((shmid = shmget((key_t) 3836, sizeof(SHM_INFOS) * 4, IPC_CREAT | 0600)) == -1) {
+    if ((shmId = shmget((key_t) 3836, sizeof(SHM_INFO) * 4, 0600 | IPC_CREAT)) == -1) {
         perror("shmget");
         exit(1);
     }
 
-    void *shared_mem = NULL;
-    if ((shared_mem = shmat(shmid, 0, SHM_RND)) ==  (void *)-1) {
+    if ((shmInfo = (SHM_INFO *) shmat(shmId, 0, SHM_RND)) == (void *) -1) {
         perror("shmat");
         exit(1);
     }
 
-    shmInfos = (SHM_INFOS *) shared_mem;
-
     while (1) {
-        for (int i = 0; i < SHM_INFO_COUNT; ++i) {
-            snprintf(shmInfos[i].str_ip, sizeof(shmInfos[i].str_ip), "1.1.1.%d", i);
-            shmInfos[i].int_ip = 12891010 + i;
-            shmInfos[i].int_id = 128 + i;
+        for (int i = 0; i < 4; ++i) {
+            snprintf(shmInfo[i].str_ip, sizeof(shmInfo[i].str_ip), "1.1.1.%d", i);
+            shmInfo[i].int_ip = 12891010 + i;
+            shmInfo[i].int_id = 128 + i;
         }
     }
 }
