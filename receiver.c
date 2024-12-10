7d52165dde55446ff09c4e4d1ca2a24a2f1b5b80
m020202 <m020202@naver.com>
2024-12-10 16:16:04 +0900

    Feat: sender & receiver communication using fifo

--- receiver.c
+++ receiver.c
@@ -10,40 +10,25 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include "shared_memory.h"
+#define MAXSIZE 64
 
-int shmid;
+int main(int argc, char **argv) {
+    char buf[MAXSIZE];
+    int fd;
 
-void sig_handler(int signo) {
-    shmctl(shmid, IPC_RMID, 0);
-    printf("\nGraceful Exit!");
-    exit(1);
-}
-
-int main() {
-    SHM_INFO *shmInfo = NULL;
-    struct sigaction act;
-
-    act.sa_handler = sig_handler;
-    sigaction(SIGINT, &act, NULL);
-
-    if ((shmid = shmget((key_t) 3836, sizeof(SHM_INFO) * 4, 0600 | IPC_CREAT)) == -1) {
-        perror("shmget");
+    if (mkfifo("fifo", 0666) == -1) {
+        perror("fifo");
         exit(1);
     }
 
-    if ((shmInfo = (SHM_INFO *) shmat(shmid, 0, SHM_RND)) == (void *) -1) {
-        perror("shmat");
+    if ((fd = open("fifo", O_RDONLY)) == -1) {
+        perror("open");
         exit(1);
     }
 
-    while(1) {
-        for (int i = 0; i < 4; ++i) {
-            printf("%d\n", i);
-            printf("%s\n", shmInfo[i].str_ip);
-            printf("%u\n", shmInfo[i].int_ip);
-            printf("%u\n", shmInfo[i].int_id);
-
-            sleep(1);
-        }
+    while(read(fd, buf, MAXSIZE) > 0) {
+        printf("Message received: %s\n", buf);
     }
+
+    return 0;
 }
--- sender.c
+++ sender.c
@@ -11,29 +11,22 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include "shared_memory.h"
-static int time_out;
+#define MAXSIZE 64
 
-void sig_handler(int signo) {
-    if (signo == SIGINT) {
-        printf("\nDo not SIGINT!\n");
-    }
-    else if (signo == SIGALRM) {
-        time_out = 1;
-    }
-}
 
-int main() {
-    struct sigaction act;
-    act.sa_handler = sig_handler;
-    sigaction(SIGINT, &act, NULL);
-    sigaction(SIGALRM, &act, NULL);
-
-    time_out = 0;
-    alarm(5);
-    while (!time_out) {
-        pause();
+int main(int argc, char** argv) {
+    int fd;
+    if ((fd = open("fifo", O_WRONLY)) == -1) {
+        perror("open");
+        exit(1);
     }
 
-    printf("Finish!!");
+    for (int i = 1; i < argc; ++i) {
+        if (write(fd, argv[i], MAXSIZE) == -1) {
+            perror("sender write");
+            exit(1);
+        }
+    }
+
     return 0;
 }
