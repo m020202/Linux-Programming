f351b8ecae1dff04ee34845afaa2a02a5ceda2c2
m020202 <m020202@naver.com>
2024-12-11 20:07:43 +0900

    Feat: process communication using shared_mem

--- receiver.c
+++ receiver.c
@@ -10,21 +10,42 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include "shared_memory.h"
+int shmid;
+
+void sig_handler(int signo) {
+    if (signo == SIGINT) {
+        printf("\nGradeful Exit!\n");
+        shmctl(shmid, IPC_RMID, 0);
+        exit(1);
+    }
+}
 
 int main(int argc, char **argv) {
-    int msqid;
+    struct sigaction act;
+    act.sa_handler = sig_handler;
+    sigaction(SIGINT, &act, NULL);
 
-    if((msqid = msgget((key_t)3836, QPERM | IPC_CREAT)) == -1) {
-        perror("msgget");
+    if ((shmid = shmget((key_t) 0x20, sizeof(SHM_INFOS) * SHM_INFO_COUNT, 0600 | IPC_CREAT)) == -1) {
+        perror("shmget");
         exit(1);
     }
 
-    struct msg_entry entry;
-    int msglen;
-    while((msglen = msgrcv(msqid, &entry, MAXLEN, 0, IPC_NOWAIT)) > 0) {
-        printf("LEN: %d\n", msglen);
-        printf("%s\n", entry.mtext);
+    void *shared_memory = shmat(shmid, 0, SHM_RND);
+    if (shared_memory == (void *) -1) {
+        perror("shmat");
+        exit(1);
     }
 
+    SHM_INFOS *shm_info = (SHM_INFOS *) shared_memory;
+
+    while (1) {
+        for (int i = 0; i < SHM_INFO_COUNT; ++i) {
+            printf("---[%d]---\n", i);
+            printf("String IP[%s]\n", shm_info[i].str_ip);
+            printf("String IP[%u]\n", shm_info[i].int_id);
+            printf("String IP[%u]\n", shm_info[i].int_ip);
+            sleep(1);
+        }
+    }
     return 0;
 }
--- sender.c
+++ sender.c
@@ -12,23 +12,91 @@
 #include <sys/sem.h>
 #include "shared_memory.h"
 
+typedef union _semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *arr;
+} semun;
 
-int main(int argc, char** argv) {
-    int msqid;
-    struct msg_entry entry;
+int initsem(key_t semkey) {
+    int semid;
 
+    if ((semid = semget(semkey, 1, 0600 | IPC_CREAT | IPC_EXCL)) == -1) {
+        if (errno == EEXIST)
+            semid = semget(semkey, 1, 0);
+        else {
+            perror("semget");
+            return -1;
+        }
+    }
+    else {
+        semun arg;
+        arg.val = 1;
+        semctl(semid, 0, SETVAL, arg);
+    }
 
-    if ((msqid = msgget((key_t) 3836, QPERM | IPC_CREAT)) == -1) {
-        perror("msgget");
+    return semid;
+}
+
+int P (int semid) {
+    struct sembuf buf;
+    buf.sem_num = 0;
+    buf.sem_op = -1;
+    buf.sem_flg = SEM_UNDO;
+
+    if (semop(semid, &buf, 1) == -1) {
+        perror("semop");
         exit(1);
     }
 
-    entry.mtype = 1;
-    strcpy(entry.mtext,"hello world");
+    return 0;
+}
 
-    if (msgsnd(msqid, &entry, MAXLEN, IPC_NOWAIT) == -1) {
-        perror("msgsnd");
+int V (int semid) {
+    struct sembuf buf;
+    buf.sem_num = 0;
+    buf.sem_op = 1;
+    buf.sem_flg = SEM_UNDO;
+
+    if (semop(semid, &buf, 1) == -1) {
+        perror("semop");
         exit(1);
+    }
+
+    return 0;
+}
+
+
+int main(int argc, char** argv) {
+    int shmid;
+    int semid;
+    key_t key = (key_t) 0x200;
+
+    if ((semid = initsem(key)) == -1) {
+        exit(1);
+    }
+
+    if ((shmid = shmget((key_t) 0x20, sizeof(SHM_INFOS) * SHM_INFO_COUNT, 0600 | IPC_CREAT)) == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    SHM_INFOS *shmInfos = NULL;
+
+    shmInfos = (SHM_INFOS *) shmat(shmid, 0, SHM_RND);
+    if (shmInfos == (SHM_INFOS *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    while (1) {
+        for (int i = 0; i < SHM_INFO_COUNT; ++i) {
+            P(semid);
+            snprintf(shmInfos[i].str_ip, sizeof(shmInfos[i].str_ip), "1.1.1.%d", i);
+            shmInfos[i].int_ip = 12891010 + i;
+            shmInfos[i].int_id = 128 + i;
+            V(semid);
+        }
     }
 
     return 0;
